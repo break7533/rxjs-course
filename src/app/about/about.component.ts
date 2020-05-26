1ce38f5cc663f0ebc8a04ce21fce30c41bee3d54
Luis Lopes <luis.lopes@blip.pt>
2020-05-26 14:09:39 +0100

    Basic timed/event observables

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { interval, timer, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,19 +11,32 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    document.addEventListener('click', evt => {
-      console.log(evt);
-    });
+  //   document.addEventListener('click', evt => {
+  //     console.log(evt);
+  //   });
 
-    let counter = 0;
-    setInterval(() => {
-      console.log(counter);
-      counter++;
-    }, 1000);
+  //   let counter = 0;
+  //   setInterval(() => {
+  //     console.log(counter);
+  //     counter++;
+  //   }, 1000);
 
-    setTimeout(() => {
-      console.log('Timeout has elapsed');
-    }, 3000);
+  //   setTimeout(() => {
+  //     console.log('Timeout has elapsed');
+  //   }, 3000);
+
+  // Interval, returns observable of numbers
+  // const interval$ = interval(1000);
+  // interval$.subscribe((val) => console.log("stream 1 " + val));
+  // interval$.subscribe((val) => console.log("stream 2 " + val));
+
+  // Timer, returns observable of numbers with a delay
+  // const timer$ = timer(3000, 1000);
+  // timer$.subscribe((val) => console.log("stream 1 " + val));
+
+  // Events, returns observable of an Event
+  // const click$ = fromEvent(document, 'click');
+  // click$.subscribe((val) => console.log(val));
+
   }
-
 }
