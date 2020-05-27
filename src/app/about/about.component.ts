51025d0e07cc59a1adac8b01351f7c72b22f5d2d
Luis Lopes <luis.lopes@blip.pt>
2020-05-27 11:46:01 +0100

    Fetch and http requests and observables

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { interval, timer, fromEvent } from 'rxjs';
+import { interval, timer, fromEvent, Observable, Observer, noop } from 'rxjs';
+import { createHttpObservable } from '../common/util';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -11,42 +13,56 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  //   document.addEventListener('click', evt => {
-  //     console.log(evt);
-  //   });
+    //   document.addEventListener('click', evt => {
+    //     console.log(evt);
+    //   });
 
-  //   let counter = 0;
-  //   setInterval(() => {
-  //     console.log(counter);
-  //     counter++;
-  //   }, 1000);
+    //   let counter = 0;
+    //   setInterval(() => {
+    //     console.log(counter);
+    //     counter++;
+    //   }, 1000);
 
-  //   setTimeout(() => {
-  //     console.log('Timeout has elapsed');
-  //   }, 3000);
+    //   setTimeout(() => {
+    //     console.log('Timeout has elapsed');
+    //   }, 3000);
 
-  // Interval, returns observable of numbers
-  // const interval$ = interval(1000);
-  // interval$.subscribe((val) => console.log("stream 1 " + val));
-  // interval$.subscribe((val) => console.log("stream 2 " + val));
+    // Interval, returns observable of numbers
+    // const interval$ = interval(1000);
+    // interval$.subscribe((val) => console.log("stream 1 " + val));
+    // interval$.subscribe((val) => console.log("stream 2 " + val));
 
-  // Timer, returns observable of numbers with a delay
-  // const timer$ = timer(3000, 1000);
-  // timer$.subscribe((val) => console.log("stream 1 " + val));
+    // Timer, returns observable of numbers with a delay
+    // const timer$ = timer(3000, 1000);
+    // timer$.subscribe((val) => console.log("stream 1 " + val));
 
-  // Events, returns observable of an Event
-  // const click$ = fromEvent(document, 'click');
-  // click$.subscribe(
-  //   (val) => console.log(val),
-  //   // Error out at any time, does not call complete
-  //   (err) => console.error(err),
-  //   // Completed, when the stream emits the last value
-  //   () => console.log('Completed'),
-  //   );
+    // Events, returns observable of an Event
+    // const click$ = fromEvent(document, 'click');
+    // click$.subscribe(
+    //   (val) => console.log(val),
+    //   // Error out at any time, does not call complete
+    //   (err) => console.error(err),
+    //   // Completed, when the stream emits the last value
+    //   () => console.log('Completed'),
+    //   );
 
-  // We cam unsubscribe at any time with the subscription returned by the subscribe
-  // const timer$ = timer(3000, 1000);
-  // const sub = timer$.subscribe((val) => console.log("stream 1 " + val));
-  // setTimeout(() => sub.unsubscribe(), 5000);
+    // We cam unsubscribe at any time with the subscription returned by the subscribe
+    // const timer$ = timer(3000, 1000);
+    // const sub = timer$.subscribe((val) => console.log("stream 1 " + val));
+    // setTimeout(() => sub.unsubscribe(), 5000);
+
+
+
+    const http$ = createHttpObservable('/api/courses');
+    
+    const courses$ = http$.pipe(
+      map((res) => Object.values(res['payload'])),
+    );
+
+    courses$.subscribe(
+        (courses) => console.log(courses),
+        noop,
+        () => console.log('completed'),
+      );
   }
 }
--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,16 @@
+import { Observable, Observer } from "rxjs";
 
+export function createHttpObservable(url: string) {
+    return Observable.create((observer: Observer<string>) => {
+      fetch(url).then((response) => {
+        return response.json();
+      }).then((body) => {
+        observer.next(body);
+        observer.complete();
+      })
+        .catch((err) => {
+          observer.error(err);
+        });
+    });
+  }
 
