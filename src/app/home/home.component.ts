2c138caa92fd6f5d970196d8df5cd70ab38fb505
Luis Lopes <luis.lopes@blip.pt>
2020-05-27 15:24:09 +0100

    Fixed callback hell, added too many requests

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from "../model/course";
-import { interval, Observable, of, timer, noop } from 'rxjs';
+import { interval, Observable, of, timer, noop, VirtualTimeScheduler } from 'rxjs';
 import { catchError, delayWhen, map, retryWhen, shareReplay, tap } from 'rxjs/operators';
 import { createHttpObservable } from '../common/util';
 
@@ -12,8 +12,8 @@ import { createHttpObservable } from '../common/util';
 })
 export class HomeComponent implements OnInit {
 
-    beginnersCourses: Course[];
-    advancedCourses: Course[];
+    beginnersCourses$: Observable<Course[]>;
+    advancedCourses$: Observable<Course[]>;
 
     constructor() {
 
@@ -23,21 +23,17 @@ export class HomeComponent implements OnInit {
 
         const http$ = createHttpObservable('/api/courses');
 
-        const courses$ = http$.pipe(
+        const courses$: Observable<Course[]> = http$.pipe(
             map((res) => Object.values(res['payload'])),
         );
 
-        courses$.subscribe(
-            (courses) => {
-                this.beginnersCourses = courses
-                    .filter((course) => course.category === 'BEGINNER');
-                this.advancedCourses = courses
-                    .filter((course) => course.category === 'ADVANCED');
-            },
-            noop,
-            () => console.log('completed'),
+        this.beginnersCourses$ = courses$.pipe(
+            map((courses) => courses.filter((course) => course.category === 'BEGINNER'))
         );
 
+        this.advancedCourses$ = courses$.pipe(
+            map((courses) =>  courses.filter((course) => course.category === 'ADVANCED'))
+        );
     }
 
 }
